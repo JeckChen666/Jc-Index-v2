edef2026924e091b4e90940d9c22d7f9e8e271e4
JeckChen <chenjie1559@gmail.com>
2024-03-03 14:38:08 +0800

    init

---
+++ src/main/java/com/jeckchen/portal/JcIndexV2Application.java
@@ -0,0 +1,13 @@
+package com.jeckchen.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcIndexV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcIndexV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeckchen/portal/JcIndexV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeckchen.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcIndexV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
